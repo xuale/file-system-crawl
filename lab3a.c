3a0bed47a6fc1017fac95f25654be09dbb292d31
Alex X Xu <xualexander1@gmail.com>
2018-11-18 13:26:18 -0800

    Read groups

--- lab3a.c
+++ lab3a.c
@@ -21,8 +21,8 @@ int img_fd;
 struct ext2_super_block superblock;
 struct ext2_group_desc *groupdesc;
 unsigned int blocksize = 0;
-unsigned int n_groups = 0;
-int *inode_bitmap;
+unsigned int group_cnt = 0;
+int *bm_inodes;
 
 void dump_error(char *message, int exit_code)
 {
@@ -50,6 +50,46 @@ void read_superblock()
          superblock.s_blocks_per_group,
          superblock.s_inodes_per_group,
          superblock.s_first_ino);
+}
+
+void read_groups()
+{
+  group_cnt = (superblock.s_blocks_count - 1) / superblock.s_blocks_per_group + 1;
+  groupdesc = malloc(group_cnt * sizeof(struct ext2_group_desc));
+
+  int c = pread(img_fd, groupdesc, group_cnt * sizeof(struct ext2_group_desc), blocksize + 1024);
+  if (c < 0)
+  {
+    dump_error("Could not load image", 2);
+  }
+
+  int i;
+  for (i = 0; i < group_cnt - 1; i++)
+  {
+    struct ext2_group_desc *gd = groupdesc + i;
+    printf("GROUP,%u,%u,%u,%u,%u,%u,%u,%u\n",
+           i,
+           superblock.s_blocks_per_group,
+           superblock.s_inodes_per_group,
+           gd->bg_free_blocks_count,
+           gd->bg_free_inodes_count,
+           gd->bg_block_bitmap,
+           gd->bg_inode_bitmap,
+           gd->bg_inode_table);
+  }
+
+  uint32_t last_block = superblock.s_blocks_count % (group_cnt - 1);
+  uint32_t last_inode = superblock.s_inodes_count % (group_cnt - 1);
+  struct ext2_group_desc *last_gd = groupdesc + group_cnt - 1;
+  printf("GROUP,%u,%u,%u,%u,%u,%u,%u,%u\n",
+           i,
+           superblock.s_blocks_per_group,
+           superblock.s_inodes_per_group,
+           last_gd->bg_free_blocks_count,
+           last_gd->bg_free_inodes_count,
+           last_gd->bg_block_bitmap,
+           last_gd->bg_inode_bitmap,
+           last_gd->bg_inode_table);
 }
 
 int main(int argc, char **argv)
